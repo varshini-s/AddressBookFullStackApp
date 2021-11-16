38b90a665b3d5eb0f61846bc843f74c787cd636c
varshini <dreamvarshini@gmail.com>
2021-11-17 01:11:40 +0530

    ADDED: application constants to use for other files

---
+++ FrontEnd/src/config/application-constants.js
@@ -0,0 +1,43 @@
+module.exports={
+     stateData : [
+        {
+          name: "Rajasthan",
+          cities: ["Bikaner", "Jaisalmer", "Jodhpur", "Udaipur", "Ajmer"]
+        },
+        {
+          name: "Maharashtra",
+          cities: ["Mumbai", "Pune", "Nagpur", "Nashik", "Aurangabad", "Solapur"]
+        },
+        {
+          name: "Karnataka",
+          cities: ["Bangalore", "Mysore", "Mangalore", "Mandya", "Hassan", "Bagalkot"]
+        },
+        {
+          name: "Assam",
+          cities: ["Guwahati", "Tezpur", "Dibrugarh", "Silchar", "North Lakhimpur"]
+        },
+        {
+          name: "Meghalaya",
+          cities: ["Cherrapunji", "Tura", "Jowai", "Baghmara", "Nongpoh"]
+        }
+      ],
+      searchItems:[
+        {
+          name: 'Bangalore'
+        },
+        {
+          name: 'Mumbai'
+        },
+        {
+          name: 'Dibrugarh'
+        },
+        {
+          name: 'Karnataka'
+        },
+        {
+          name: 'Maharashtra'
+        }
+      ]
+}
+    
+
