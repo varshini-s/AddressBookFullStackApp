886f6c37193e3f7042258e0ec3533b1b2c4a5ad7
varshini <dreamvarshini@gmail.com>
2021-11-17 00:54:07 +0530

    ADDED: hello world display

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import AddressForm from './components/address-form/address-form.jsx'
+import HomePage from './components/home-page/home-page'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,Redirect
+} from "react-router-dom"
+
+
+function App() {
+  return (
+    <div className="App">
+      hello world
+    </div>
+  );
+}
+
+
+export default App;
